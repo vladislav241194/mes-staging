50d6ef1a0e4957262fe5c5795310a96dc9e294c0
vladislav241194 <vladislav241194@gmail.com>
2026-07-19 20:48:13 +0300

    chore: bump Pilot release to v.1.499.99

--- src/app.js
+++ src/app.js
@@ -183,7 +183,7 @@ const renderMesModulePatternPage = createMesModulePatternRenderer({
   renderUiModuleSidebar,
 });
 
-const APP_VERSION_FALLBACK = "v.1.499.98";
+const APP_VERSION_FALLBACK = "v.1.499.99";
 const APP_VERSION = (
   typeof window !== "undefined"
   && typeof window.__MES_DEPLOY_VERSION__ === "string"
